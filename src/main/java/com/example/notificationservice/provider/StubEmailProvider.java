ed783927ef73ae2acf655f393ae814c14cd3f9ee
Andrey Kuznetsov <daniilbacardi@yandex.ru>
2026-03-21 17:23:42 +0300

    tests

--- src/main/java/com/example/notificationservice/provider/StubEmailProvider.java
+++ src/main/java/com/example/notificationservice/provider/StubEmailProvider.java
@@ -6,15 +6,25 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
 @Component
 public class StubEmailProvider implements EmailProvider {
 
     private static final Logger log = LoggerFactory.getLogger(StubEmailProvider.class);
 
+    private final Set<String> failOnceProcessedNotificationIds = ConcurrentHashMap.newKeySet();
+
     @Override
     public void send(NotificationMessage message) {
         if ("FORCE_RETRY".equals(message.getTemplateCode())) {
             throw new RetryableProviderException("Temporary email provider failure");
+        }
+
+        if ("FAIL_ONCE_THEN_SUCCESS".equals(message.getTemplateCode())
+                && failOnceProcessedNotificationIds.add(message.getNotificationId())) {
+            throw new RetryableProviderException("Temporary email provider failure on first attempt");
         }
 
         log.info(
